60816250aa8ae4741b2bbb788a9c2061f57f28a0
dipam2612 <dipam2612@gmail.com>
2022-05-15 18:21:25 +0530

    core java basic program

---
+++ CoreJavaBasic/src/simpleprintprograms/demo.java
@@ -0,0 +1,11 @@
+package simpleprintprograms;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("jewalaaas kaa");
+		   System.out.print("hao re bhau 2345!@#$%^*9");
+	}
+
+}
